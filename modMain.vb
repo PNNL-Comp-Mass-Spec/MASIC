866995e4f435bd7cd2c1c8d0c4d66f8e63639141
alchemistmatt <matthew.monroe@pnnl.gov>
2015-06-22 12:33:05 -0700

    Change AnyCPU to be the default platform for compiling

--- modMain.vb
+++ modMain.vb
@@ -8,7 +8,7 @@ Option Strict On
 ' Copyright 2005, Battelle Memorial Institute.  All Rights Reserved.
 
 ' E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com
-' Website: http://panomics.pnnl.gov/ or http://www.sysbio.org/resources/staff/
+' Website: http://omics.pnl.gov/ or http://www.sysbio.org/resources/staff/ or http://panomics.pnnl.gov/
 ' -------------------------------------------------------------------------------
 ' 
 ' Licensed under the Apache License, Version 2.0; you may not use this file except
@@ -383,7 +383,7 @@ Public Module modMain
 			Console.WriteLine()
 
 			Console.WriteLine("E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com")
-			Console.WriteLine("Website: http://panomics.pnnl.gov/ or http://omics.pnl.gov")
+			Console.WriteLine("Website: http://omics.pnl.gov/ or http://panomics.pnnl.gov/")
 			Console.WriteLine()
 
 			' Delay for 750 msec in case the user double clicked this file from within Windows Explorer (or started the program via a shortcut)
