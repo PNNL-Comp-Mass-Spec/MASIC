42ee428adf268db44aadc6c9a02415b430454d1e
alchemistmatt <matthew.monroe@pnnl.gov>
2017-03-29 20:24:44 -0700

    Add modMain

---
+++ MASICBrowser/modMain.vb
@@ -0,0 +1,191 @@
+﻿Option Strict On
+
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Reflection
+Imports PRISM
+Imports SharedVBNetRoutines
+
+' -------------------------------------------------------------------------------
+' Written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA)
+' Program started October 17, 2003
+' Copyright 2005, Battelle Memorial Institute.  All Rights Reserved.
+
+' E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com
+' Website: http://omics.pnl.gov/ or http://www.sysbio.org/resources/staff/ or http://panomics.pnnl.gov/
+' -------------------------------------------------------------------------------
+'
+' Licensed under the Apache License, Version 2.0; you may not use this file except
+' in compliance with the License.  You may obtain a copy of the License at
+' http://www.apache.org/licenses/LICENSE-2.0
+
+Module modMain
+
+    Public Const PROGRAM_DATE As String = "March 29, 2017"
+
+    Private mInputFilePath As String
+
+    Public Function Main() As Integer
+        ' Returns 0 if no error, error code if an error
+
+        Dim objParseCommandLine As New clsParseCommandLine
+        Dim blnProceed As Boolean
+
+        mInputFilePath = String.Empty
+
+        Try
+            blnProceed = False
+            If objParseCommandLine.ParseCommandLine Then
+                If SetOptionsUsingCommandLineParameters(objParseCommandLine) Then blnProceed = True
+            End If
+
+            If objParseCommandLine.NeedToShowHelp OrElse Not blnProceed Then
+                ShowProgramHelp()
+                Return -1
+            End If
+
+            ShowGUI()
+
+            Return 0
+
+        Catch ex As Exception
+            ShowErrorMessage("Error occurred in modMain->Main: " & Environment.NewLine & ex.Message)
+            Return -1
+        End Try
+
+    End Function
+
+    ''' <summary>
+    ''' Returns the full path to the executing .Exe or .Dll
+    ''' </summary>
+    ''' <returns>File path</returns>
+    ''' <remarks></remarks>
+    Private Function GetAppPath() As String
+        Return Assembly.GetExecutingAssembly().Location
+    End Function
+
+
+    ''' <summary>
+    ''' Returns the .NET assembly version followed by the program date
+    ''' </summary>
+    ''' <param name="strProgramDate"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function GetAppVersion(strProgramDate As String) As String
+        Return Assembly.GetExecutingAssembly().GetName().Version.ToString() & " (" & strProgramDate & ")"
+    End Function
+
+
+    Private Function SetOptionsUsingCommandLineParameters(objParseCommandLine As clsParseCommandLine) As Boolean
+        ' Returns True if no problems; otherwise, returns false
+
+        Dim strValue As String = String.Empty
+        Dim lstValidParameters = New List(Of String) From {"I"}
+
+        Try
+            ' Make sure no invalid parameters are present
+            If objParseCommandLine.InvalidParametersPresent(lstValidParameters) Then
+                ShowErrorMessage("Invalid commmand line parameters",
+                  (From item In objParseCommandLine.InvalidParameters(lstValidParameters) Select "/" + item).ToList())
+                Return False
+            Else
+
+                ' Query objParseCommandLine to see if various parameters are present
+                With objParseCommandLine
+                    If .RetrieveValueForParameter("I", strValue) Then
+                        mInputFilePath = strValue
+                    ElseIf .NonSwitchParameterCount > 0 Then
+                        ' Treat the first non-switch parameter as the input file
+                        mInputFilePath = .RetrieveNonSwitchParameter(0)
+                    End If
+
+                End With
+
+                Return True
+            End If
+
+        Catch ex As Exception
+            ShowErrorMessage("Error parsing the command line parameters: " & Environment.NewLine & ex.Message)
+        End Try
+
+        Return False
+
+    End Function
+
+    Private Sub ShowErrorMessage(strMessage As String)
+        Dim strSeparator = "------------------------------------------------------------------------------"
+
+        Console.WriteLine()
+        Console.WriteLine(strSeparator)
+        Console.WriteLine(strMessage)
+        Console.WriteLine(strSeparator)
+        Console.WriteLine()
+
+    End Sub
+
+    Private Sub ShowErrorMessage(strTitle As String, items As List(Of String))
+        Dim strSeparator = "------------------------------------------------------------------------------"
+        Dim strMessage As String
+
+        Console.WriteLine()
+        Console.WriteLine(strSeparator)
+        Console.WriteLine(strTitle)
+        strMessage = strTitle & ":"
+
+        For Each item As String In items
+            Console.WriteLine("   " + item)
+            strMessage &= " " & item
+        Next
+        Console.WriteLine(strSeparator)
+        Console.WriteLine()
+
+    End Sub
+
+    Public Sub ShowGUI()
+
+        System.Windows.Forms.Application.EnableVisualStyles()
+        System.Windows.Forms.Application.DoEvents()
+
+        Dim masicBrowser = New frmBrowser()
+
+        if Not String.IsNullOrWhiteSpace(mInputFilePath) then
+            masicBrowser.FileToAutoLoad = mInputFilePath
+        End If
+        masicBrowser.ShowDialog()
+
+    End Sub
+
+    Private Sub ShowProgramHelp()
+
+        Try
+
+            Console.WriteLine("This program is used to visualize the results from MASIC")
+            Console.WriteLine()
+
+            Console.WriteLine("Program syntax:" & Environment.NewLine & Path.GetFileName(GetAppPath()))
+            Console.WriteLine(" MasicResults_SICs.xml")
+            Console.WriteLine()
+
+            Console.WriteLine("The input file path is the MASIC results file to load")
+            Console.WriteLine()
+
+            Console.WriteLine("Program written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA) in 2017")
+            Console.WriteLine("Version: " & GetAppVersion(PROGRAM_DATE))
+
+            Console.WriteLine()
+
+            Console.WriteLine("E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com")
+            Console.WriteLine("Website: http://omics.pnl.gov/ or http://panomics.pnnl.gov/")
+            Console.WriteLine()
+
+            ' Delay for 750 msec in case the user double clicked this file from within Windows Explorer (or started the program via a shortcut)
+            Threading.Thread.Sleep(750)
+
+        Catch ex As Exception
+            ShowErrorMessage("Error displaying the program syntax: " & ex.Message)
+        End Try
+
+    End Sub
+
+
+End Module
