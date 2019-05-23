85093e7f7925d62f0af874cb5a6c546ebf97627d
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-23 16:13:38 -0700

    Update the Warning message to emphasize that interference scores will be 1 if no data was found in the isolation window centered around the precursor ion

--- DataInput/clsDataImport.vb
+++ DataInput/clsDataImport.vb
@@ -334,7 +334,8 @@ Namespace DataInput
                     precursorMissingPct = mPrecursorNotFoundCount / CDbl(scanList.FragScans.Count) * 100
                 End If
 
-                OnWarningEvent(String.Format("Could not determine the precursor for {0:F1}% of the MS2 spectra ({1} / {2} scans)",
+                OnWarningEvent(String.Format("Could not determine the precursor for {0:F1}% of the MS2 spectra ({1} / {2} scans). " &
+                                             "These scans will have an Interference Score of 1 (to avoid accidentally filtering out low intensity results).",
                                              precursorMissingPct, mPrecursorNotFoundCount, scanList.FragScans.Count))
             End If
 
