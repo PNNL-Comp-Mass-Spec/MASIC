9911cae176b4e95b475cffcd3fa231ee4fd77148
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-04 18:30:58 -0700

    Update batch file and add dictionary words

--- MASIC_Installer/MASIC_Console_ZipFiles.bat
+++ MASIC_Installer/MASIC_Console_ZipFiles.bat
@@ -1,3 +1,3 @@
-"C:\Program Files\7-Zip\7z.exe" a Output\MASIC_Console_Program.zip ..\bin\Console\Debug\MASIC_Console.exe ..\bin\Console\Debug\MASIC_Console.pdb ..\bin\Console\Debug\*.dll ..\Readme.md  ..\RevisionHistory.txt ..\Docs\MASICParameters.xml ..\Lib\netcdf.dll ..\Lib\RawFileReaderLicense.doc ..\Docs\CustomMZList_AcqTime.txt ..\Docs\CustomMZList.txt ..\Docs\LTQ_Example\Default_2008-08-22.xml ..\Docs\QExactive_Example\LTQ-FT_10ppm_2014-08-06.xml
+"C:\Program Files\7-Zip\7z.exe" a Zipped\MASIC_Console_Program.zip ..\bin\Console\Debug\MASIC_Console.exe ..\bin\Console\Debug\MASIC_Console.pdb ..\bin\Console\Debug\*.dll ..\Readme.md  ..\RevisionHistory.txt ..\Docs\MASICParameters.xml ..\Lib\netcdf.dll ..\Lib\RawFileReaderLicense.doc ..\Docs\CustomMZList_AcqTime.txt ..\Docs\CustomMZList.txt ..\Docs\LTQ_Example\Default_2008-08-22.xml ..\Docs\QExactive_Example\LTQ-FT_10ppm_2014-08-06.xml
 
 pause
