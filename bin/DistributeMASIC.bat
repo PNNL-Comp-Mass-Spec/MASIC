401680a303d2185c87e18492f2dd065332a6dc7b
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-21 15:25:29 -0700

    Update the distribution file

--- bin/DistributeMASIC.bat
+++ bin/DistributeMASIC.bat
@@ -2,13 +2,13 @@
 
 echo.
 echo Copying to C:\DMS_Programs
-xcopy MASIC.exe C:\DMS_Programs\MASIC /D /Y
-xcopy *.dll     C:\DMS_Programs\MASIC /D /Y
+xcopy Console\Debug\MASIC_Console.exe C:\DMS_Programs\MASIC /D /Y
+xcopy Console\Debug\*.dll             C:\DMS_Programs\MASIC /D /Y
 
 echo.
 echo Copying to \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution
-xcopy MASIC.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
-xcopy *.dll     \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
+xcopy Console\Debug\MASIC_Console.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
+xcopy Console\Debug\*.dll             \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
 
 echo.
 pause
