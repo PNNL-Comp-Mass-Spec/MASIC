6ed444323bcab6451e8aaca893e1c9125b2ee9b3
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-26 17:21:35 -0700

    Remove 7-zip command since in other batch file

--- MASIC_Installer/Distribute_Files.bat
+++ MASIC_Installer/Distribute_Files.bat
@@ -8,6 +8,4 @@ xcopy ..\bin\*.dll                       \\floyd\software\MASIC\Exe_Only\ /D /Y
 xcopy ..\MASICBrowser\bin\MASICBrowser.* \\floyd\software\MASIC\Exe_Only\ /D /Y
 xcopy ..\MASICBrowser\bin\*.dll          \\floyd\software\MASIC\Exe_Only\ /D /Y
 
-"C:\Program Files\7-Zip\7z.exe" e -o\\floyd\software\MASIC\MASIC_Console\ Output\MASIC_Console_Program.zip
-
 pause
