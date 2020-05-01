c75d0f948ce3c779e4a1b155f26fdb827c0a4676
alchemistmatt <matthew.monroe@pnnl.gov>
2020-05-01 10:29:15 -0700

    Update batch files to use the release version

--- MASIC_Installer/Distribute_Files.bat
+++ MASIC_Installer/Distribute_Files.bat
@@ -1,11 +1,19 @@
+@echo off
+echo.
+echo Be sure to build in Release mode
+pause
+@echo on
+
 xcopy Output\MASIC_Installer.exe \\floyd\software\MASIC\ /D /Y
 xcopy ..\Readme.md               \\floyd\software\MASIC\ /D /Y
 xcopy ..\RevisionHistory.txt     \\floyd\software\MASIC\ /D /Y
 
 
-xcopy ..\bin\MASIC.*                     \\floyd\software\MASIC\Exe_Only\ /D /Y
-xcopy ..\bin\*.dll                       \\floyd\software\MASIC\Exe_Only\ /D /Y
-xcopy ..\MASICBrowser\bin\MASICBrowser.* \\floyd\software\MASIC\Exe_Only\ /D /Y
-xcopy ..\MASICBrowser\bin\*.dll          \\floyd\software\MASIC\Exe_Only\ /D /Y
+xcopy ..\bin\Release\MASIC.*                     \\floyd\software\MASIC\Exe_Only\ /D /Y
+xcopy ..\bin\Release\*.dll                       \\floyd\software\MASIC\Exe_Only\ /D /Y
+xcopy ..\MASICBrowser\bin\Release\MASICBrowser.* \\floyd\software\MASIC\Exe_Only\ /D /Y
+xcopy ..\MASICBrowser\bin\Release\*.dll          \\floyd\software\MASIC\Exe_Only\ /D /Y
+
+xcopy ..\bin\Console\Release\MASIC_Console.*     \\floyd\software\MASIC\Exe_Only\ /D /Y
 
 pause
--- MASIC_Installer/MASIC_Console_ZipFiles.bat
+++ MASIC_Installer/MASIC_Console_ZipFiles.bat
@@ -1,3 +1,7 @@
-"C:\Program Files\7-Zip\7z.exe" a Zipped\MASIC_Console_Program.zip ..\bin\Console\Debug\MASIC_Console.exe ..\bin\Console\Debug\MASIC_Console.pdb ..\bin\Console\Debug\*.dll ..\Readme.md  ..\RevisionHistory.txt ..\Docs\MASICParameters.xml ..\Lib\netcdf.dll ..\Lib\RawFileReaderLicense.doc ..\Docs\CustomMZList_AcqTime.txt ..\Docs\CustomMZList.txt ..\Docs\LTQ_Example\Default_2008-08-22.xml ..\Docs\QExactive_Example\LTQ-FT_10ppm_2014-08-06.xml
+"C:\Program Files\7-Zip\7z.exe" a Zipped\MASIC_Console_Program.zip ..\bin\Console\Release\MASIC_Console.exe ..\bin\Console\Release\MASIC_Console.pdb ..\bin\Console\Release\*.dll ..\Readme.md  ..\RevisionHistory.txt ..\Docs\MASICParameters.xml ..\Lib\RawFileReaderLicense.doc ..\Docs\CustomMZList_AcqTime.txt ..\Docs\CustomMZList.txt ..\Docs\LTQ_Example\Default_2008-08-22.xml ..\Docs\QExactive_Example\LTQ-FT_10ppm_2014-08-06.xml
+
+cd Zipped
+"C:\Program Files\7-Zip\7z.exe" a MASIC_Installer.zip ..\Output\MASIC_Installer.exe ..\..\Readme.md
+cd ..
 
 pause
