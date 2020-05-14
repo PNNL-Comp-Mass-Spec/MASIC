a0d22f0980994363cd2a083342c5018bb48a2c12
alchemistmatt <matthew.monroe@pnnl.gov>
2020-05-14 14:27:59 -0700

    Update batch file

--- Docs/MRM_Example/ProcessData.bat
+++ Docs/MRM_Example/ProcessData.bat
@@ -1,1 +1,2 @@
 ..\..\bin\Console\Debug\MASIC_Console.exe QC18PepsR1_4Apr18.raw /p:Default_2008-08-22.xml /o:Compare
+..\..\bin\Console\Release\MASIC_Console.exe QC18PepsR1_4Apr18.raw /p:Default_2008-08-22.xml /o:CompareRelease
