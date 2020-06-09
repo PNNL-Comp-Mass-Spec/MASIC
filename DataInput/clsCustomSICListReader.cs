8fb8d3a71223b29a6cbc7e76f63fc558d89218eb
alchemistmatt <matthew.monroe@pnnl.gov>
2020-06-09 15:56:25 -0700

    Rename enum

--- DataInput/clsCustomSICListReader.cs
+++ DataInput/clsCustomSICListReader.cs
@@ -17,7 +17,7 @@ namespace MASIC.DataInput
         public const string CUSTOM_SIC_COLUMN_TIME_TOLERANCE = "TimeTolerance";
         public const string CUSTOM_SIC_COLUMN_COMMENT = "Comment";
 
-        private enum eCustomSICFileColumns
+        private enum CustomSICFileColumns
         {
             MZ = 0,
             MZToleranceDa = 1,
@@ -81,16 +81,16 @@ namespace MASIC.DataInput
 
                 // Keys in this dictionary are column identifier
                 // Values are the index of this column in the tab-delimited text file (-1 if not present)
-                var columnMap = new Dictionary<eCustomSICFileColumns, int>();
+                var columnMap = new Dictionary<CustomSICFileColumns, int>();
 
-                var columnNamesByIdentifier = new Dictionary<eCustomSICFileColumns, SortedSet<string>>();
-                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, eCustomSICFileColumns.MZ, CUSTOM_SIC_COLUMN_MZ);
-                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, eCustomSICFileColumns.MZToleranceDa, CUSTOM_SIC_COLUMN_MZ_TOLERANCE);
-                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, eCustomSICFileColumns.ScanCenter, CUSTOM_SIC_COLUMN_SCAN_CENTER);
-                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, eCustomSICFileColumns.ScanTolerance, CUSTOM_SIC_COLUMN_SCAN_TOLERANCE);
-                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, eCustomSICFileColumns.ScanTime, CUSTOM_SIC_COLUMN_SCAN_TIME);
-                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, eCustomSICFileColumns.TimeTolerance, CUSTOM_SIC_COLUMN_TIME_TOLERANCE);
-                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, eCustomSICFileColumns.Comment, CUSTOM_SIC_COLUMN_COMMENT);
+                var columnNamesByIdentifier = new Dictionary<CustomSICFileColumns, SortedSet<string>>();
+                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, CustomSICFileColumns.MZ, CUSTOM_SIC_COLUMN_MZ);
+                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, CustomSICFileColumns.MZToleranceDa, CUSTOM_SIC_COLUMN_MZ_TOLERANCE);
+                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, CustomSICFileColumns.ScanCenter, CUSTOM_SIC_COLUMN_SCAN_CENTER);
+                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, CustomSICFileColumns.ScanTolerance, CUSTOM_SIC_COLUMN_SCAN_TOLERANCE);
+                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, CustomSICFileColumns.ScanTime, CUSTOM_SIC_COLUMN_SCAN_TIME);
+                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, CustomSICFileColumns.TimeTolerance, CUSTOM_SIC_COLUMN_TIME_TOLERANCE);
+                DataTableUtils.AddColumnNamesForIdentifier(columnNamesByIdentifier, CustomSICFileColumns.Comment, CUSTOM_SIC_COLUMN_COMMENT);
 
                 if (!File.Exists(customSICValuesFileName))
                 {
@@ -134,7 +134,7 @@ namespace MASIC.DataInput
                             DataTableUtils.GetColumnMappingFromHeaderLine(columnMap, dataLine, columnNamesByIdentifier);
 
                             // Make sure that, at a minimum, the MZ column is present
-                            if (DataTableUtils.GetColumnIndex(columnMap, eCustomSICFileColumns.MZ) < 0 )
+                            if (DataTableUtils.GetColumnIndex(columnMap, CustomSICFileColumns.MZ) < 0 )
                             {
                                 var errorMessage = "Custom M/Z List file " + customSICValuesFileName + "does not have a column header named " + CUSTOM_SIC_COLUMN_MZ + " in the first row; this header is required (valid column headers are: " + GetCustomMZFileColumnHeaders() + ")";
                                 ReportError(errorMessage);
@@ -143,8 +143,8 @@ namespace MASIC.DataInput
                                 return false;
                             }
 
-                            if (DataTableUtils.GetColumnIndex(columnMap, eCustomSICFileColumns.ScanTime) >= 0 &&
-                                DataTableUtils.GetColumnIndex(columnMap, eCustomSICFileColumns.TimeTolerance) >= 0)
+                            if (DataTableUtils.GetColumnIndex(columnMap, CustomSICFileColumns.ScanTime) >= 0 &&
+                                DataTableUtils.GetColumnIndex(columnMap, CustomSICFileColumns.TimeTolerance) >= 0)
                             {
                                 forceAcquisitionTimeMode = true;
                                 mCustomSICList.ScanToleranceType = clsCustomSICList.eCustomSICScanTypeConstants.AcquisitionTime;
@@ -171,19 +171,19 @@ namespace MASIC.DataInput
                         };
 
 
-                        var mzTarget = DataTableUtils.GetColumnValue(dataCols, columnMap, eCustomSICFileColumns.MZ, string.Empty, out var mzValuePresent);
+                        var mzTarget = DataTableUtils.GetColumnValue(dataCols, columnMap, CustomSICFileColumns.MZ, string.Empty, out var mzValuePresent);
                         if (mzValuePresent)
                         {
                             mzSearchSpec.MZ = ParseDouble(mzTarget, "MZ", linesRead);
                         }
 
-                        var mzToleranceDa = DataTableUtils.GetColumnValue(dataCols, columnMap, eCustomSICFileColumns.MZToleranceDa, string.Empty, out var mzTolerancePresent);
+                        var mzToleranceDa = DataTableUtils.GetColumnValue(dataCols, columnMap, CustomSICFileColumns.MZToleranceDa, string.Empty, out var mzTolerancePresent);
                         if (mzTolerancePresent)
                         {
                             mzSearchSpec.MZToleranceDa = ParseDouble(mzToleranceDa, "MZToleranceDa", linesRead);
                         }
 
-                        var scanCenter = DataTableUtils.GetColumnValue(dataCols, columnMap, eCustomSICFileColumns.ScanCenter, string.Empty, out var scanCenterPresent);
+                        var scanCenter = DataTableUtils.GetColumnValue(dataCols, columnMap, CustomSICFileColumns.ScanCenter, string.Empty, out var scanCenterPresent);
                         if (scanCenterPresent)
                         {
                             // Do not use this value if both the ScanTime and the TimeTolerance columns were present
@@ -193,7 +193,7 @@ namespace MASIC.DataInput
                             }
                         }
 
-                        var scanTolerance = DataTableUtils.GetColumnValue(dataCols, columnMap, eCustomSICFileColumns.ScanTolerance, string.Empty, out var scanTolPresent);
+                        var scanTolerance = DataTableUtils.GetColumnValue(dataCols, columnMap, CustomSICFileColumns.ScanTolerance, string.Empty, out var scanTolPresent);
                         if (scanTolPresent)
                         {
                             // Do not use this value if both the ScanTime and the TimeTolerance columns were present
@@ -211,7 +211,7 @@ namespace MASIC.DataInput
                             }
                         }
 
-                        var scanTime = DataTableUtils.GetColumnValue(dataCols, columnMap, eCustomSICFileColumns.ScanTime, string.Empty, out var scanTimePresent);
+                        var scanTime = DataTableUtils.GetColumnValue(dataCols, columnMap, CustomSICFileColumns.ScanTime, string.Empty, out var scanTimePresent);
                         if (scanTimePresent)
                         {
                             // Only use this value if both the ScanTime and the TimeTolerance columns were present
@@ -221,7 +221,7 @@ namespace MASIC.DataInput
                             }
                         }
 
-                        var timeTolerance = DataTableUtils.GetColumnValue(dataCols, columnMap, eCustomSICFileColumns.TimeTolerance, string.Empty, out var timeTolerancePresent);
+                        var timeTolerance = DataTableUtils.GetColumnValue(dataCols, columnMap, CustomSICFileColumns.TimeTolerance, string.Empty, out var timeTolerancePresent);
                         if (timeTolerancePresent)
                         {
                             // Only use this value if both the ScanTime and the TimeTolerance columns were present
@@ -231,7 +231,7 @@ namespace MASIC.DataInput
                             }
                         }
 
-                        mzSearchSpec.Comment = DataTableUtils.GetColumnValue(dataCols, columnMap, eCustomSICFileColumns.Comment);
+                        mzSearchSpec.Comment = DataTableUtils.GetColumnValue(dataCols, columnMap, CustomSICFileColumns.Comment);
 
                         mCustomSICList.AddMzSearchTarget(mzSearchSpec);
                     }
