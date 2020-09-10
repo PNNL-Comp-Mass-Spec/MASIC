74a8c4329c78e6d7ab358c11f12c5d131029e39f
alchemistmatt <matthew.monroe@pnnl.gov>
2020-09-09 17:09:28 -0700

    Add global suppression files

---
+++ MASICPeakFinder/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.",
+    Justification = "This design pattern is used to silently handle several types of non-fatal errors", Scope = "module")]
+
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses aren't needed", Scope = "module")]
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "module")]
