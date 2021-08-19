dffce2c09a9db3fee27d07afee7b6847aaff0251
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-19 10:39:19 -0700

    Update program dates

--- MASICBrowser/Program.cs
+++ MASICBrowser/Program.cs
@@ -24,7 +24,7 @@ namespace MASICBrowser
 
     internal static class Program
     {
-        public const string PROGRAM_DATE = "August 18, 2021";
+        public const string PROGRAM_DATE = "August 19, 2021";
 
         private static string mInputFilePath;
 
