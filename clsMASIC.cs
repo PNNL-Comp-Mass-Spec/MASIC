e74d7bd99efbe5d26ebd2183d1820f25eabac147
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-07 16:15:15 -0800

    Update program dates

--- clsMASIC.cs
+++ clsMASIC.cs
@@ -46,7 +46,7 @@ namespace MASIC
         /// </summary>
         public clsMASIC()
         {
-            mFileDate = "January 5, 2022";
+            mFileDate = "January 7, 2022";
 
             LocalErrorCode = MasicErrorCodes.NoError;
             StatusMessage = string.Empty;
