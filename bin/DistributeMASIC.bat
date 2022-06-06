b34812516b56745197cf8b6b2f350cbf828fd094
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-06 15:03:17 -0700

    Build and distribute the .pdb file

--- bin/DistributeMASIC.bat
+++ bin/DistributeMASIC.bat
@@ -8,6 +8,7 @@ echo Copying to C:\DMS_Programs
 @echo on
 
 xcopy Console\Release\MASIC_Console.exe C:\DMS_Programs\MASIC /D /Y
+xcopy Console\Release\MASIC_Console.pdb C:\DMS_Programs\MASIC /D /Y
 xcopy Console\Release\*.dll             C:\DMS_Programs\MASIC /D /Y
 xcopy Console\Release\*.py              C:\DMS_Programs\MASIC /D /Y
 xcopy ..\Readme.md                      C:\DMS_Programs\MASIC /D /Y
@@ -19,6 +20,7 @@ echo Copying to \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribut
 @echo on
 
 xcopy Console\Release\MASIC_Console.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
+xcopy Console\Release\MASIC_Console.pdb \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
 xcopy Console\Release\*.dll             \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
 xcopy Console\Release\*.py              \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
 xcopy ..\Readme.md                      \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MASIC /D /Y
