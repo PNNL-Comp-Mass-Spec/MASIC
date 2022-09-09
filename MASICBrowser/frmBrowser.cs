b2e9454c0673a9385ff3c8a7e9c42f878cb0813d
alchemistmatt <matthew.monroe@pnnl.gov>
2022-09-08 21:15:36 -0700

    Update program date and add dictionary word

--- MASICBrowser/frmBrowser.cs
+++ MASICBrowser/frmBrowser.cs
@@ -44,7 +44,7 @@ namespace MASICBrowser
         // ReSharper disable once CommentTypo
         // Ignore Spelling: cancelled, const, Ctrl, frag, fragscan, Golay, Loc, resmooth, Savitzky, similarFragScan
 
-        private const string PROGRAM_DATE = "June 14, 2022";
+        private const string PROGRAM_DATE = "September 8, 2022";
 
         /// <summary>
         /// Constructor
